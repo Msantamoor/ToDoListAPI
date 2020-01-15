033dc79e54bff000005be7ba6f601f113e20eb1c
Morgan Santamoor <msantamoor@gmail.com>
2020-01-15 13:48:36 -0700

    fixed the fix

---
+++ routes/taskRoutes.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var taskDal = require('/Users/morgansantamoor/Helio/Projects/server/taskDAL.js');
+
+router.get('/*', function (req, res, next) {
+    taskDal.testConnection()
+    res.send('success');
+});
+
+
+
+module.exports = router; 
