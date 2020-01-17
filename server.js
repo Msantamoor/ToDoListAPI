bec28b930da3d8e261c6f9e71c4bf732f4ea1bf5
Morgan Santamoor <msantamoor@gmail.com>
2020-01-17 15:20:30 -0700

    cors removal test 2

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cors = require('cors')
 const path = require('path')
 require('dotenv').config()
 
@@ -10,7 +9,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3307;
 
-// app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'react')));
