81b455ff0de315b6584e29db994f18388bfc5b2d
Morgan Santamoor <msantamoor@gmail.com>
2020-01-17 15:18:05 -0700

    cors removal test

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3307;
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'react')));
